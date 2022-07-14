96441bd0e64e1f237ea72077fd6eb030f147e2f2
krallice <sroz9960@gmail.com>
2022-07-14 21:39:16 +1000

    Basic Lexer

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,20 @@
 #include <fstream>
 #include <sstream>
 
+enum class BFTOKEN {
+  INCREMENT_PTR,
+  DECREMENT_PTR,
+
+  INCREMENT_DATA,
+  DECREMENT_DATA,
+
+  OUTPUT,
+  INPUT,
+  
+  START_LOOP,
+  END_LOOP,
+};
+
 std::string ReadFileIntoString(const std::string& path) {
     auto ss = std::ostringstream{};
     std::ifstream input_file(path);
@@ -16,6 +30,42 @@ std::string ReadFileIntoString(const std::string& path) {
     return ss.str();
 }
 
+int lexer(const std::string& input, std::vector<BFTOKEN>& tokens) {
+
+  for(std::string::size_type i = 0; i < input.size(); i++) {
+
+    switch(input[i]) {
+      case '.':
+        tokens.push_back(BFTOKEN::OUTPUT);
+        break;
+      case ',':
+        tokens.push_back(BFTOKEN::INPUT);
+        break;
+      case '>':
+        tokens.push_back(BFTOKEN::INCREMENT_PTR);
+        break;
+      case '<':
+        tokens.push_back(BFTOKEN::DECREMENT_PTR);
+        break;
+      case '+':
+        tokens.push_back(BFTOKEN::INCREMENT_DATA);
+        break;
+      case '-':
+        tokens.push_back(BFTOKEN::DECREMENT_DATA);
+        break;
+      case '[':
+        tokens.push_back(BFTOKEN::START_LOOP);
+        break;
+      case ']':
+        tokens.push_back(BFTOKEN::END_LOOP);
+        break;
+      default:
+        break;
+    }
+  }
+  return 0;
+}
+
 int main(int argc, char** argv) {
 
   std::string program;
@@ -24,6 +74,9 @@ int main(int argc, char** argv) {
   } catch (...) {
     program = ">++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<++.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-]<+.";
   }
-  
+
+  std::vector<BFTOKEN> tokens;
+  int lexerret = lexer(program, tokens);
+
   return 0;
 }
